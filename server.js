9f547ef4099fa8dad6460617f8c60c3afb62b693
Richard Dotchin <richard.dotchin@gmail.com>
2017-01-18 14:56:28 -0500

    update HTML and begin work on server.js

--- server.js
+++ server.js
@@ -2,3 +2,17 @@ var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
 var path = require('path');
+var PORT = 3000;
+
+//GET home.html
+app.get('/', function(req, res){
+	res.sendFile(__dirname + '/app/public/home.html')
+});
+//GET survey.html
+app.get('/survey' function(req, res){
+	res.sendFile(__dirname + 'app/public/survey.html')
+})
+//listen on port 3000
+app.listen(PORT, function(){
+	console.log("listening on port http://localhost:" + PORT);
+});
