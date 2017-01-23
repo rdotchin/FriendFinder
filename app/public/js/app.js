086f6585b6196d8dc4c4fec19d2bd409f1b45eba
Richard Dotchin <richard.dotchin@gmail.com>
2017-01-23 12:43:48 -0500

    changes to app.js to clear form and chosen when user submits and fixed modal

--- app/public/js/app.js
+++ app/public/js/app.js
@@ -2,36 +2,52 @@ $(document).ready(function(){
 //add chosen to the survey
 $(".chosen-select").chosen({})
 
+
 $('#submit-button').on('click', function(){
-		
+		// Form validation
+    	function validateForm() {
+		  var isValid = true;
+		  $('.form-control').each(function() {
+		    if ( $(this).val() === '' )
+		        isValid = false;
+		  });
+
+		  $('.chosen-select').each(function() {
+
+		  	if( $(this).val() === "")
+		  		isValid = false
+		  })
+		  return isValid;
+		}
+
+		// If all required fields are filled
+		if (validateForm() == true) {		
 	
-//create an object of the person taking the survey to be added to the friends object
+			//create an object of the person taking the survey to be added to the friends object
+			//add the name and image to the object
+			var userData = {
+				name: $('#survey-name').val().trim(),
+				photo: $('#survey-image').val().trim(),
+				scores: [$("#q1").val(), $("#q2").val(), $("#q3").val(), $("#q4").val(), $("#q5").val(), $("#q6").val(), $("#q7").val(), $("#q8").val(), $("#q9").val(), $("#q10").val() ]
+		    	};
 
-	//add the name and image to the object
-	var userData = {
-		name: $('#survey-name').val().trim(),
-		image: $('#survey-image').val().trim(),
-		scores: [$("#q1").val(), $("#q2").val(), $("#q3").val(), $("#q4").val(), $("#q5").val(), $("#q6").val(), $("#q7").val(), $("#q8").val(), $("#q9").val(), $("#q10").val() ]
-    	};
+			var currentURL = window.location.origin;
 
-	/*//add the numbers in the response of the survey to the object
-	$('.chosen-select').each(function(index, element){
-		//return the elements value and set it to a number for comparison later
-		userData[element.id] = Number($(element).val());
-	});*/
-
-	var currentURL = window.location.origin;
-
-	$.post(currentURL + "/api/friends", userData, function(data){
-		console.log(data);
-		console.log(data.name);
-		console.log(data.image);
-		$("#friend-name").append(data.name);
-		$("#friend-image").attr("src", data.photo);
-		$("#resultsModal").modal('show');
-		
-	});	
-
+			$.post(currentURL + "/api/friends", userData, function(data){
+				console.log(data);
+				console.log(data.name);
+				console.log(data.image);
+				$("#friend-name").append(data.name);
+				$("#friend-image").attr("src", data.photo);
+				$("#resultsModal").modal('show');
+				//empty chosen survey, name field and image field
+				$(".chosen-select").val('').trigger("chosen:updated");
+				$("#survey-name").val(" ");
+				$("#survey-image").val(" ");
+			});	
+			} else {
+			alert("Please fill out all fields!")
+		}
 });
 
 
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -3,100 +3,95 @@
 var path = require('path');
 //requires the data from the friends.js file that includes the friends variable
 var friendsData = require('../data/friends.js');
-//hold the userArray survey arrayNumber results to compare to other scores
+
+/*=============================================COMPARISON VARIABLES=========================================*/
+//user scores are passed into this array to be compared to friends
 var userArray = []
-//starting arrayNumber to compare different between the user score and the current friend
+/*the number compared to the difference set to a high number so it will always be replaced 
+by the first friend comparison*/
 var comparisonNum = 500;
 
-//variable set to hold the arrayNumber of the best friend to be returned
+//variable set to hold the best friend object from teh friendsData array to be returned
 var bestFriend;
+//variable to hold the current difference betwen the user/friend scores
 var difference = 0;
-var arrayNum = 0
+//current friend number being compared from the friendsData array
+var friendNum = 0
 /*sets the required data from friends.js to the variable friends so it can be 
 called in app.get*/
+
 
 //exporting from survey.js the express app function
 module.exports = function(app){
 	//displays the variable friends data (from the friends.js file) in JSON format
 	app.get('/api/friends', function(req, res){
-		//display the friends data in json format
 		res.json(friendsData);
 	});
 
-
-
+	/*create a POST route to take in the users data, compare it to friends, and return the best friend*/
 	app.post("/api/friends", function(req, res){
-		
-		//turns string into a number and pushes it in the userArray
+		/*turns string of numbers into integers and pushes it in the userArray to be compared with friends*/
 		for(var i = 0; i <req.body.scores.length; i++){
 			userArray.push(parseInt(req.body.scores[i]))
-
-			
 		}
 
 		
-	//variable set to hold the friendNumber of the best friend to be returned
-var bestFriend = 0;
-var difference = 0;
-var friendNum = 0;
-	console.log(friendsData.length);
+/*==================================================FUNCTIONS==================================================*/
+		/*once all friends have been compared to ther user this function is called to send the
+		best friends data to the HTML/app.js to be displayed in modal.  It also calls the reset function
+		to reset all the comparison variables for the next user submisssion*/
 		function finalFriend(){
 			res.send(bestFriend);
 			reset();
-			console.log("final best friend" + bestFriend.name);
 		}
 		
 		
-		//compare user vs friend
+		/*function to compare the current friend vs the user data then pass that into the bestScore()*/
 		function compareScores(){
+			/*if all the friends have been compared to the user then it calls the finalFriend function.  
+			else it resets the difference number to 0 and compares the user vs current friend score.  The 
+			number is kept positive by using Math.abs().  Then the bestScore function is called passing in
+		 	the difference and current friend number*/
 			if(friendNum == friendsData.length ){
-				//once all friends are compared the function to return the details of the best friend is called
 				finalFriend();
 			}
 			else{
-			console.log("========================")
-			console.log("friendNum: " + friendNum)
 			difference = 0;
-			for(var i = 0; i < userArray.length; i++){
-				difference += Math.abs(friendsData[friendNum].scores[i] - userArray[i]);
+				for(var i = 0; i < userArray.length; i++){
+					difference += Math.abs(friendsData[friendNum].scores[i] - userArray[i]);
+				}
+				bestScore(difference, friendNum);
 			}
-			console.log("comp friend: " + friendsData[friendNum].name);
-			console.log("diff " + difference)
-			bestScore(difference, friendNum);
-		}
 		}
 
-		//if current friend score is less than previous he becomes the new best friend
+		/*function to compare how the current friend/user comparison compares to the rest*/
 		function bestScore(difference, friendfriendNum) {
+			//increment friendNum by 1 to compare next friend to user
 			friendNum +=1;
-			
+			/*if the difference is less than the comparison number that friend becomes the new bestFriend
+			then compareScores() is called to compare the next user*/
 			if (difference < comparisonNum){
 				bestFriend = friendsData[friendfriendNum]
 				comparisonNum = difference;
-				console.log("comp num " + comparisonNum)
-				console.log("current best friend : " + bestFriend.name)
-				/*friendNum += 1;*/
 				compareScores();	
 			} else {
-				console.log("current best friend : " + bestFriend.name)
-				console.log("comp num " + comparisonNum)
-				/*friendNum += 1;*/
 				compareScores();
 			}
 		}
-		compareScores();
 		
-		//push the users info to the friends array and reset all the variables to compare the next user
+		/*pushes the users information to the friendsData array.  It also resets all of the variables for the next
+		user*/
 		function reset(){
 			friendsData.push(req.body);
 			difference = 0;
-			arrayNum = 0;
+			friendNum = 0;
 			comparisonNum = 500;
 			bestFriend;
 			userArray = [];
 		}
 
-
+		/*start the comparison once a user submits their information in survey.html*/
+		compareScores();
 
 	}); //app.post end
 
